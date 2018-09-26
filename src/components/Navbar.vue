9f4942991a0b19064cc11f5b6270089a512e2b29
Jam <jamjam@jaminpie.me>
2018-09-27 02:03:21 +0800

    login log out added

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,7 +3,51 @@
         <div class="nav-wrapper green">
             <div class="container">
                 <router-link to="/" class="brand-logo">Bot Manager</router-link>
+                <ul class="right">
+                    <li v-if="isLoggedIn">
+                        <router-link to="/">Dashboard</router-link>
+                    </li>
+                    <li v-if="!isLoggedIn">
+                        <router-link to="/login">Login</router-link>
+                    </li>
+                    <li v-if="!isLoggedIn">
+                        <router-link to="/register">Register</router-link>
+                    </li>
+                    <li v-if="isLoggedIn">
+                        <button v-on:click="logout" class="btn black">Logout</button>
+                    </li>
+                </ul>
             </div>
         </div>
     </nav>
 </template>
+
+<script>
+import firebase from 'firebase';
+
+export default {
+    name: 'navbar',
+    data() {
+        return {
+            isLoggedIn: false,
+            currentUser: false
+        }
+    },
+    created() {
+        if (firebase.auth().currentUser) {
+            this.isLoggedIn = true;
+            this.currentUser = firebase.auth().currentUser.email;
+        }
+    },
+    methods: {
+        logout: function() {
+            firebase.auth().signOut().then( () => {
+                console.log("LOGGED OUT")
+                this.$router.go({
+                    path: this.$router.path
+                });
+            })
+        }
+    }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -4,31 +4,104 @@ import Dashboard from '@/components/Dashboard'
 import NewBot from '@/components/NewBot'
 import ViewBot from '@/components/ViewBot'
 import EditBot from '@/components/EditBot'
+import Login from '@/components/Login'
+import Register from '@/components/Register'
+
+import firebase from 'firebase'
 
 Vue.use(Router)
 
-export default new Router({
+let router = new Router({
   routes: [
     {
       path: '/',
       name: 'dashboard',
-      component: Dashboard
+      component: Dashboard,
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login,
+      meta: {
+        requiresGuest: true
+      }
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: Register,
+      meta: {
+        requiresGuest: true
+      }
     },
     {
       path: '/new',
       name: 'new-bot',
-      component: NewBot
+      component: NewBot,
+      meta: {
+        requiresAuth: true
+      }
     },
     {
       path: '/edit/:bot_id',
       name: 'edit-bot',
-      component: EditBot
+      component: EditBot,
+      meta: {
+        requiresAuth: true
+      }
     },
     ,
     {
       path: '/:bot_id',
       name: 'view-bot',
-      component: ViewBot
+      component: ViewBot,
+      meta: {
+        requiresAuth: true
+      }
     }
   ]
 })
+
+router.beforeEach( (to, from, next) => {
+  //Check for requiredAuth guard
+  if( to.matched.some(record => record.meta.requiresAuth) ) {
+    //Check if NOT logged in
+    if( !firebase.auth().currentUser ) {
+      //Go to login
+      next({
+        path: '/login',
+        query: {
+          redirect: to.fullPath
+        }
+      })
+    } else {
+      // Process to route
+      next();
+    }
+  } else if ( to.matched.some(record => record.meta.requiresGuest) ) {
+    //Check if IS logged in
+    if( firebase.auth().currentUser ) {
+      //Go to path
+      console.log("IS USER")
+      next({
+        path: '/',
+        query: {
+          redirect: to.fullPath
+        }
+      })
+    } else {
+      // Process to route
+      next();
+    }
+  } else {
+    // Process to route
+    next();
+  }
+})
+//Nav Guard
+
+
+export default router
