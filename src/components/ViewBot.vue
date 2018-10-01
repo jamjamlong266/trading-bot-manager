6f530d710905e207c0ad5d7f4836895882a599f1
Jam <jamjam@jaminpie.me>
2018-10-01 17:23:59 +0800

    update view vue

--- src/components/ViewBot.vue
+++ src/components/ViewBot.vue
@@ -5,19 +5,28 @@
                 <h4>{{ indicator }}</h4>
             </li>
             <li class="collection-item">
-                Bot Indicator: {{ indicator }}
+                <p>
+                    Bot Indicator: {{ indicator }}
+                </p>
+                <div>
+                    <p>Overbought : {{ overbought_value }}</p>
+                    <p>Oversold : {{ oversold_value }}</p>
+                </div>
             </li>
             <li class="collection-item">
-                Exchange Platform: {{ exchange }}
+                <p>Entry Value : {{ entry_value }}</p>
+                <p>Amount : {{ amount }}</p>
+
+                <p>Exit Value : {{ exit_value }}</p>
+                <p>Percentage : {{ percentage_value }}</p>
             </li>
             <li class="collection-item">
-                API Key: {{ api_key }}
+                <p>Exchange Platform: {{ exchange }}</p>
+                <p>API Key: {{ api_key }}</p>
+                <p>Secret Key: {{ secret_key }}</p>
             </li>
             <li class="collection-item">
-                Secret Key: {{ secret_key }}
-            </li>
-            <li class="collection-item">
-                Trading Pair: {{ trading_pair }}
+                <p>Trading Pair: {{ trading_pair }}</p>
             </li>
         </ul>
         <router-link to="/" class="btn grey">Back</router-link>
@@ -38,12 +47,22 @@ export default {
     name: 'view-bot',
     data () {
         return {
-            bot_id: null,
-            exchange: null,
             indicator: null,
+            overbought_value: null,
+            oversold_value: null,
+            entry_value: null,
+            amount:null,
+            exit_value: null,
+            percentage_value: null,
+            stop_value: null,
+            exchange: null,
             api_key: null,
             secret_key: null,
             trading_pair: null,
+            ema_value1: null,
+            ema_value2:null,
+            bot_id: null,
+            uid: null,
         }
     },
     beforeRouteEnter (to, from, next) {
@@ -51,12 +70,22 @@ export default {
         .then(snapshot => {
             snapshot.forEach(doc => {
                 next(vm => {
-                    vm.bot_id = doc.data().bot_id,
-                    vm.exchange = doc.data().exchange,
                     vm.indicator = doc.data().indicator,
+                    vm.overbought_value = doc.data().overbought_value,
+                    vm.oversold_value = doc.data().oversold_value,
+                    vm.entry_value = doc.data().entry_value,
+                    vm.amount = doc.data().amount,
+                    vm.exit_value = doc.data().exit_value,
+                    vm.percentage_value = doc.data().percentage_value,
+                    vm.stop_value = doc.data().stop_value,
+                    vm.exchange = doc.data().exchange,
                     vm.api_key = doc.data().api_key,
-                    vm.secret_key = doc.data().secret_key
-                    vm.trading_pair = doc.data().trading_pair
+                    vm.secret_key = doc.data().secret_key,
+                    vm.trading_pair = doc.data().trading_pair,
+                    vm.ema_value1 = doc.data().ema_value1,
+                    vm.ema_value2 = doc.data().ema_value2,
+                    vm.bot_id = doc.data().bot_id,
+                    vm.uid = doc.data().uid
                 })
             })
         })
@@ -69,12 +98,22 @@ export default {
             db.collection('trading_bot').where('bot_id', '==', this.$route.params.bot_id).get()
             .then(snapshot => {
                 snapshot.forEach( doc => {
-                    this.bot_id = doc.data().bot_id,
-                    this.exchange = doc.data().exchange,
                     this.indicator = doc.data().indicator,
+                    this.overbought_value = doc.data().overbought_value,
+                    this.oversold_value = doc.data().oversold_value,
+                    this.entry_value = doc.data().entry_value,
+                    this.amount = doc.data().amount,
+                    this.exit_value = doc.data().exit_value,
+                    this.percentage_value = doc.data().percentage_value,
+                    this.stop_value = doc.data().stop_value,
+                    this.exchange = doc.data().exchange,
                     this.api_key = doc.data().api_key,
-                    this.secret_key = doc.data().secret_key
-                    this.trading_pair = doc.data().trading_pair
+                    this.secret_key = doc.data().secret_key,
+                    this.trading_pair = doc.data().trading_pair,
+                    this.ema_value1 = doc.data().ema_value1,
+                    this.ema_value2 = doc.data().ema_value2,
+                    this.bot_id = doc.data().bot_id,
+                    this.uid = doc.data().uid
                 })
             })
         },
